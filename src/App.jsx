2e5e5896ba0f79c8021e09297c45830a994d77da
LONESOME <pasterbasher@gmail.com>
2024-11-06 17:33:30 +0700

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './Components/Home'
+import User from './Components/User';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+function handleClick(){
+  User
+}
+
+  return (
+    Home()
+    <button onClick={handleClick}>Admin Sector</button>
+  )
+}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+        Generation Thailand
+        React - Assessment
+    </div>
+    <button>Admin Sector</button>
+  )
+}
+
+export default Home
